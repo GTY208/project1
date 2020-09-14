adcfad980a6d7bd63ae1c93e1caece67ec15b7d7
Gideon <2101121@talnet.nl>
2020-09-14 20:43:11 +0200

    Project1 DATABASE

--- sql_statements.sql/project1.sql
+++ sql_statements.sql/project1.sql
@@ -1,16 +1,22 @@
-create table account (
-ID int NOT NULL AUTO_INCREMENT,
-email varchar(250),
-password varchar(250),
 
-primary key (ID)
+-- met CREATE TABLE maakte ik de tabellen aan genaamd account en persoon
+-- met PRIMARY KEY statement heb ik dus van ID in beide kolommen een primary key gemaakt 
+-- met NOT NULL achter ID geef ik dus aan dat die geen enkele waarde mag accepteren 
+
+CREATE TABLE account (                   
+    ID int NOT NULL AUTO_INCREMENT,
+    email VARCHAR (250),
+    password VARCHAR (250),
+    
+    PRIMARY KEY (ID)
+    
 );
 
-create table persoon (
-ID int NOT NULL AUTO_INCREMENT,
-firstName varchar(250),
-lastName varchar(250),
-userName varchar(250),
-
-primary key (ID)
+CREATE TABLE persoon (
+    ID int NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR (250),
+    lastName VARCHAR (250),
+    userName VARCHAR (250),
+    
+    PRIMARY KEY (ID)
 );
